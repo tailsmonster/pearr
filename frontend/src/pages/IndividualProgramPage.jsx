1a16d5dbb799b48a89dd6267af0b8a7e59f72905
nico <lotailsmonsieur62c900@gmail.com>
2024-05-16 15:35:29 -0400

    jnwfhw

--- frontend/src/pages/IndividualProgramPage.jsx
+++ frontend/src/pages/IndividualProgramPage.jsx
@@ -5,31 +5,45 @@ import { getProgramById } from "../adapters/program-adapter";
 
 
 const IndividualProgramPage = () => {
-    const {id} = useParams();
-    // const breed = breed.find((breed) => breed.name = breedName)
+  const {id} = useParams();
+  // const breed = breed.find((breed) => breed.name = breedName)
 
-    const [programInfo, setProgramInfo] = useState([])
-    const [error, setError] = useState('')
+  const [programInfo, setProgramInfo] = useState([])
+  const [error, setError] = useState('')
 
-    useEffect(() => {
-        const getProgramInfo = async () => {
-            const program = await getProgramById(id);
-            if (program) setProgramInfo(program[0]);
-            console.log(program)
-        }
-        getProgramInfo();
-    }, [])
+  useEffect(() => {
+    const getProgramInfo = async () => {
+        const program = await getProgramById(id);
+        if (program) setProgramInfo(program[0]);
+        console.log(program)
+    }
+    getProgramInfo();
+  }, [])
 
 
-    console.log(programInfo)
-    return (
-        <>
-            <h2 id="program-title">{programInfo.name}</h2>
-            <div id="program-info">
-              <img src="" alt="" />
-            </div>
-        </>
-    )
+  console.log(programInfo)
+  return (
+      <>
+      <section id='info'>
+        <h2 id="program-title">{programInfo.name}</h2>
+        <div id="p1">
+          <img src={programInfo.imgUrl} alt={`${programInfo} picture!`} />
+        </div>
+        <div id="p2">
+          <h4>About:</h4>
+          <p>{programInfo.bio}</p>
+          <br />
+          <h4>Location:</h4>
+          <p>{programInfo.borough}</p>
+          
+          <h4>Website:</h4>
+          <a useRef={programInfo.websiteUrl}>{programInfo.websiteUrl}</a>
+        </div>
+
+
+      </section>
+    </>
+  )
 };
 
 export default IndividualProgramPage;

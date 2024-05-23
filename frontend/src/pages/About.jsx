8273c31f5b9ac27436774696b39cd444c4ec0e3e
CrisM05 <Max42113@gmail.com>
2024-05-23 11:21:44 -0400

    About us styling

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import groupPhoto from "../assets/Group-Photo.png"
+import "./About.css"
 
 const AboutUs = () => {
   return (
@@ -6,11 +8,11 @@ const AboutUs = () => {
       <div className="container">
         <div className="columns is-centered">
           <div className="column is-8 abt-box">
-            <h1 className="title font has-text-centered abt-us-txt" >About Us</h1>
+            <h1 className="title font has-text-centered abt-us-txt" id="about-us-header">About Us</h1>
             <div style={{margin: '0 20px'}}>
             <div className="content">
               <h2 className="subtitle font" style={{fontSize:'35px', margin: '10px 0', textAlign: 'center'}}>Project Purpose</h2>
-              <p className="abt-text">
+              <p className="abt-text normal-font">
                 PEAR NYC is a project aimed at connecting low-income families in
                 New York City with affordable and enriching extracurricular
                 activities for their children. Our goal is to bridge the
@@ -18,7 +20,7 @@ const AboutUs = () => {
                 programs that support the development and well-being of young
                 individuals.
               </p>
-              <p>
+              <p className="abt-text normal-font">
                 We understand the challenges faced by low-income families in
                 navigating the complex landscape of extracurricular activities.
                 PEAR NYC serves as a centralized platform that curates and
@@ -32,10 +34,10 @@ const AboutUs = () => {
                 <h2 className="subtitle font" style={{fontSize:'35px', margin: '10px 0', textAlign: 'center'}}>Our Team</h2>
               <div className="column">
                 <figure className="image is-3by2" style={{display: 'flex', justifyContent: 'center', padding: '20px 0'}}>
-                  <img src="https://media.discordapp.net/attachments/1232393999294140529/1242615128835489853/IMG_2810.png?ex=664f23c5&is=664dd245&hm=de354378f84aadae9bc46c0a91c1794f495c6a175458e1cea355e66b244f3289&=&format=webp&quality=lossless&width=960&height=443" alt="Group Picture" />
+                  <img src={groupPhoto} alt="Group Picture" />
                 </figure>
               </div>
-                <p>
+                <p className="abt-text normal-font team">
                   We are a dedicated team of individuals passionate about making
                   a positive impact in our community. With diverse backgrounds
                   and expertise, we have come together to create PEAR NYC and

02ec9327e7359e2f63fd00d9ca977f060376cc95
Nico <aroca.twelve@proton.me>
2025-09-16 13:22:51 -0400

    init postgres

---
+++ backend/src/index.js
@@ -0,0 +1,19 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+// test route
+app.get("/", (req, res) => {
+  res.json({ message: "Hello from pearr backend 🚀" });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
